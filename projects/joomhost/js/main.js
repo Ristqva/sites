890bcb7630599e83e45d2289b600a47e4be0a485
Ristqva <74404079+Ristqva@users.noreply.github.com>
2021-02-12 10:53:28 +0300

    Add files via upload

---
+++ projects/joomhost/js/main.js
@@ -0,0 +1,15 @@
+$(function() {
+
+    $('.reviews-slider').slick({
+        infinite: true,
+        slidesToShow: 2,
+        slidesToScroll: 2,
+        arrows: false,
+        dots: true
+    });
+
+    $('.btn__menu').on('click', function() {
+        $('.menu__list').slideToggle();
+    });
+
+});
